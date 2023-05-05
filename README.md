a12af658003efb2aeda533f732b1319d35ada26c
andre <aaa.newwork@gmail.com>
2023-05-05 21:44:36 +0300

    create and read .ini file

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # telebot_homehelper
 Telegram bot for my private tasks
 started at 05-05-2023
+
+--- INSTALL ---
+pip install pyTelegramBotAPI
--- purchase/purchase.py
+++ purchase/purchase.py
@@ -1,1 +1,16 @@
+import configparser
+import telebot
 
+
+config = configparser.ConfigParser()  # создаём объекта парсера
+config.read("settings.ini")  # читаем конфиг
+token = config['Telegram']['token'] # обращаемся как к обычному словарю!
+print(token)
+
+bot=telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	bot.send_message(message.chat.id,"Привет ✌️ ")
+
+bot.infinity_poling()
